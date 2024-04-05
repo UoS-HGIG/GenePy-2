287c2d48c6556ec8302b6f7aeff97e70189c616e
gc1a20 <65593824+gc1a20@users.noreply.github.com>
2024-04-05 17:01:42 +0100

    Add files via upload

--- make_scores_mat.py
+++ make_scores_mat.py
@@ -1,172 +1,207 @@
-# -*- coding: utf-8 -*-
-
-import sys, re
 import numpy as np
 import pandas as pd
+from numba import njit, cuda, prange
+import math
+import pyarrow.csv as pa_csv
+from typing import Tuple
+import argparse
 
 
-# IMPORT data passed through the 1st argument;followed by the Gene_ENSGid as the 2nd
+def read_meta_file(filepath: str) -> pd.DataFrame:
 
-data=pd.read_csv(sys.argv[1],sep='\t',header=0,dtype='object')
-gene=sys.argv[2]
-CADD=sys.argv[3]
-header=data.columns.values
-data = np.array(data)
+    parse_options = pa_csv.ParseOptions(delimiter="\t")
+    read_options = pa_csv.ReadOptions(block_size=1e9)
+    data = pa_csv.read_csv(
+        filepath, parse_options=parse_options, read_options=read_options
+    )
+    data = data.to_pandas()
+    return data
 
-##cadd score; reformat CADD range to 0-1
-## the UKBB 200k cohort raw variant has CADD16 ranges from -18.793437 to 19.100986 including the prescore and permutated score (GRCh38_v1.6;VEP 100_GRCh38)
-scores = data[:,16:25]
-scores = scores.astype('float')
-scores = (scores - (-19.811548))/(25.028523-(-19.811548))
-scores[np.isnan(scores)] = 0
 
-##allele frequency as it is in the UKBB cohort; this is currently based on the raw pVCF data.
-af = data[:,6:15]
-af = af.astype('float')
-##the ref allele frequency
-af0 = 1-np.nansum(af,axis=1)
-af[np.isnan(af)] = 1
+def get_scores(data: np.array) -> np.array:
+    ##cadd score; reformat CADD range to 0-1
+    ## the UKBB 200k cohort raw variant has CADD16 ranges from -18.793437 to 19.100986 including the prescore and permutated score (GRCh38_v1.6;VEP 100_GRCh38)
+    scores = data[:, 16:25]
+    scores = scores.astype("float")
+    scores = (scores - (-19.811548)) / (25.028523 - (-19.811548))
+    scores[np.isnan(scores)] = 0
+    s0 = scores[:, 0]
+    scores = np.insert(scores, 0, s0, axis=1)
+    scores[np.isnan(scores)] = 0
 
-##sample genotype data as it is in format of allele_1/allele_2
-samples=data[:,26:]
-samples_header=header[26:]
+    return scores
 
-##Clalculate GenePy score; assuming genotype 0/0 has a score=0
+
+def get_allele_freq(data: np.array) -> np.array:
+    ##allele frequency as it is in the UKBB cohort; this is currently based on the raw pVCF data.
+    af = data[:, 6:15]
+    af = af.astype("float")
+    ##the ref allele frequency
+    af0 = 1 - np.nansum(af, axis=1)
+    af0 = af0[:, np.newaxis]
+    af[np.isnan(af)] = 1
+    af = np.hstack([af0, af])
+
+    return af
+
+
+def format_data(data: pd.DataFrame) -> Tuple[np.array, np.array, np.array, np.array]:
+    header = data.columns.values
+    data = np.array(data)
+    scores = get_scores(data=data)
+    af = get_allele_freq(data=data)
+    samples_header = header[26:]
+    samples = data[:, 26:]
+    samples[samples == "0"] = "0/0"
+    samples = samples.astype("str")
+
+    return scores, af, samples, samples_header
+
+
+def parse_command_line_args():
+    parser = argparse.ArgumentParser(description="GenePy2 - Make score matrix")
+    parser.add_argument("--gene", type=str, help="Gene name", required=True)
+    parser.add_argument("--cadd", type=str, help="CADD score", required=True)
+    parser.add_argument("--gpu", action="store_true", help="Use GPU")
+    parser.add_argument(
+        "--gpu-threads",
+        type=int,
+        help="Number of threads in each GPU block",
+        nargs="?",
+        const=256,
+        default=256,
+    )
+    args = parser.parse_args()
+    return args
+
+
+@njit()
+def get_score(S: np.array, af: np.array, db1: np.array, s_int: np.array):
+    for i in range(db1.shape[0]):
+        for j in prange(db1.shape[1]):
+            if s_int[i, j, 0] == 0:
+                db1[i][j] = S[i, s_int[i][j][1]] * (
+                    -math.log10(af[i, s_int[i][j][0]] * af[i, s_int[i][j][1]])
+                )
+
+            elif s_int[i, j, 1] == 0:
+                db1[i][j] = S[i, s_int[i][j][0]] * (
+                    -math.log10(af[i, s_int[i][j][0]] * af[i, s_int[i][j][1]])
+                )
+            else:
+                db1[i][j] = (
+                    (S[i, s_int[i][j][0]] + S[i, s_int[i][j][1]])
+                    * 0.5
+                    * (-math.log10(af[i, s_int[i][j][0]] * af[i, s_int[i][j][1]]))
+                )
+    return db1
+
+
+@cuda.jit
+def get_score_kernel(S_d, db1_d, af_d, s_int_d):
+    start = cuda.grid(1)
+    stride = cuda.gridsize(1)
+
+    for i in range(db1_d.shape[0]):
+        for j in range(start, db1_d.shape[1], stride):
+
+            if s_int_d[i, j, 0] == 0:
+                db1_d[i][j] = S_d[i, s_int_d[i][j][1]] * (
+                    -math.log10(af_d[i, s_int_d[i][j][0]] * af_d[i, s_int_d[i][j][1]])
+                )
+
+            elif s_int_d[i, j, 1] == 0:
+                db1_d[i][j] = S_d[i, s_int_d[i][j][0]] * (
+                    -math.log10(af_d[i, s_int_d[i][j][0]] * af_d[i, s_int_d[i][j][1]])
+                )
+            else:
+                db1_d[i][j] = (
+                    (S_d[i, s_int_d[i][j][0]] + S_d[i, s_int_d[i][j][1]])
+                    * 0.5
+                    * (-math.log10(af_d[i, s_int_d[i][j][0]] * af_d[i, s_int_d[i][j][1]]))
+                )
+
+
+def get_scores_cuda(
+    scores: np.array,
+    af: np.array,
+    db1: np.array,
+    samples_int: np.array,
+    threads_per_block: int,
+):
+
+    num_cols = db1.shape[1]
+    S_d = cuda.to_device(np.ascontiguousarray(scores))
+    db1_d = cuda.to_device(np.ascontiguousarray(db1))
+    af_d = cuda.to_device(np.ascontiguousarray(af))
+    s_int_d = cuda.to_device(np.ascontiguousarray(samples_int))
+
+    blockspergrid = (num_cols + threads_per_block - 1) // threads_per_block
+    get_score_kernel[blockspergrid, threads_per_block](S_d, db1_d, af_d, s_int_d)
+
+    S_d = None
+    af_d = None
+    s_int_d = None
+    db1 = db1_d.copy_to_host()
+    db1_d = None
+
+    return db1
+
+
+def index(data: np.array) -> np.array:
+    int_data = data.ravel().view("uint8")[::4]
+    int_data = int_data.reshape(data.shape[0], -1, 3) - 48
+    int_data[(int_data[:, :, 2] > 253) | (int_data[:, :, 0] > 253)] = [0, 0, 0]
+    int_data = int_data[:, :, [0, 2]].reshape(int_data.shape[0], int_data.shape[1], 2)
+    return int_data.astype("uint8")
+
+
 def nan_if(arr, value):
     return np.where(arr == value, np.nan, arr)
 
-def score_db(samples,scores,af0,af):
-    S=np.copy(scores)
-    db1=np.copy(samples)
-    af0=np.copy(af0)
-    af=np.copy(af)
-    out1=[]
-    for i in range(db1.shape[0]):
-        db1[i][db1[i]=='0/0']=0
-        db1[i][db1[i]=='0']=0
-        db1[i][db1[i]=='./0']=0
-        db1[i][db1[i]=='./.']=0
-        db1[i][db1[i]=='./1']=0
-        db1[i][db1[i]=='./2']=0
-        db1[i][db1[i]=='./3']=0
-        db1[i][db1[i]=='./4']=0
-        db1[i][db1[i]=='./5']=0
-        db1[i][db1[i]=='./6']=0
-        db1[i][db1[i]=='./7']=0
-        db1[i][db1[i]=='./8']=0
-        db1[i][db1[i]=='./9']=0
-        db1[i][db1[i]=='10/']=0
-        db1[i][db1[i]=='11/']=0
-        db1[i][db1[i]=='12/']=0
-        db1[i][db1[i]=='13/']=0
-        db1[i][db1[i]=='14/']=0
-        db1[i][db1[i]=='15/']=0
-        db1[i][db1[i]=='16/']=0
-        db1[i][db1[i]=='17/']=0
-        db1[i][db1[i]=='18/']=0
-        db1[i][db1[i]=='19/']=0
-        db1[i][db1[i]=='20/']=0
-        db1[i][db1[i]=='21/']=0
-        db1[i][db1[i]=='22/']=0
-        db1[i][db1[i]=='23/']=0
-        db1[i][db1[i]=='24/']=0
-        db1[i][db1[i]=='25/']=0
-        db1[i][db1[i]=='26/']=0
-        db1[i][db1[i]=='27/']=0
-        db1[i][db1[i]=='28/']=0
-        db1[i][db1[i]=='30/']=0
-        db1[i][db1[i]=='0/1']=S[i,0]*(-np.log10((af0[i])*(af[i,0])))
-        db1[i][db1[i]=='1/0']=S[i,0]*(-np.log10((af0[i])*(af[i,0])))
-        db1[i][db1[i]=='0/2']=S[i,1]*(-np.log10((af0[i])*(af[i,1])))
-        db1[i][db1[i]=='2/0']=S[i,1]*(-np.log10((af0[i])*(af[i,1])))
-        db1[i][db1[i]=='0/3']=S[i,2]*(-np.log10((af0[i])*(af[i,2])))
-        db1[i][db1[i]=='3/0']=S[i,2]*(-np.log10((af0[i])*(af[i,2])))
-        db1[i][db1[i]=='0/4']=S[i,3]*(-np.log10((af0[i])*(af[i,3])))
-        db1[i][db1[i]=='4/0']=S[i,3]*(-np.log10((af0[i])*(af[i,3])))
-        db1[i][db1[i]=='0/5']=S[i,4]*(-np.log10((af0[i])*(af[i,4])))
-        db1[i][db1[i]=='5/0']=S[i,4]*(-np.log10((af0[i])*(af[i,4])))
-        db1[i][db1[i]=='0/6']=S[i,5]*(-np.log10((af0[i])*(af[i,5])))
-        db1[i][db1[i]=='6/0']=S[i,5]*(-np.log10((af0[i])*(af[i,5])))
-        db1[i][db1[i]=='0/7']=S[i,6]*(-np.log10((af0[i])*(af[i,6])))
-        db1[i][db1[i]=='7/0']=S[i,6]*(-np.log10((af0[i])*(af[i,6])))
-        db1[i][db1[i]=='0/8']=S[i,7]*(-np.log10((af0[i])*(af[i,7])))
-        db1[i][db1[i]=='8/0']=S[i,7]*(-np.log10((af0[i])*(af[i,7])))
-        db1[i][db1[i]=='0/9']=S[i,8]*(-np.log10((af0[i])*(af[i,8])))
-        db1[i][db1[i]=='9/0']=S[i,8]*(-np.log10((af0[i])*(af[i,8])))
-        db1[i][db1[i]=='1/1']=S[i,0]*(-np.log10((af[i,0])*(af[i,0])))
-        db1[i][db1[i]=='1/2']=((S[i,0]+S[i,1])/2)*(-np.log10((af[i,0])*(af[i,1])))
-        db1[i][db1[i]=='2/1']=((S[i,0]+S[i,1])/2)*(-np.log10((af[i,0])*(af[i,1])))
-        db1[i][db1[i]=='1/3']=((S[i,0]+S[i,2])/2)*(-np.log10((af[i,0])*(af[i,2])))
-        db1[i][db1[i]=='3/1']=((S[i,0]+S[i,2])/2)*(-np.log10((af[i,0])*(af[i,2])))
-        db1[i][db1[i]=='1/4']=((S[i,0]+S[i,3])/2)*(-np.log10((af[i,0])*(af[i,3])))
-        db1[i][db1[i]=='4/1']=((S[i,0]+S[i,3])/2)*(-np.log10((af[i,0])*(af[i,3])))
-        db1[i][db1[i]=='1/5']=((S[i,0]+S[i,4])/2)*(-np.log10((af[i,0])*(af[i,4])))
-        db1[i][db1[i]=='5/1']=((S[i,0]+S[i,4])/2)*(-np.log10((af[i,0])*(af[i,4])))
-        db1[i][db1[i]=='1/6']=((S[i,0]+S[i,5])/2)*(-np.log10((af[i,0])*(af[i,5])))
-        db1[i][db1[i]=='6/1']=((S[i,0]+S[i,5])/2)*(-np.log10((af[i,0])*(af[i,5])))
-        db1[i][db1[i]=='1/7']=((S[i,0]+S[i,6])/2)*(-np.log10((af[i,0])*(af[i,6])))
-        db1[i][db1[i]=='7/1']=((S[i,0]+S[i,6])/2)*(-np.log10((af[i,0])*(af[i,6])))
-        db1[i][db1[i]=='1/8']=((S[i,0]+S[i,7])/2)*(-np.log10((af[i,0])*(af[i,7])))
-        db1[i][db1[i]=='8/1']=((S[i,0]+S[i,7])/2)*(-np.log10((af[i,0])*(af[i,7])))
-        db1[i][db1[i]=='1/9']=((S[i,0]+S[i,8])/2)*(-np.log10((af[i,0])*(af[i,8])))
-        db1[i][db1[i]=='9/1']=((S[i,0]+S[i,8])/2)*(-np.log10((af[i,0])*(af[i,8])))
-        db1[i][db1[i]=='2/2']=S[i,1]*(-np.log10((af[i,1])*(af[i,1])))
-        db1[i][db1[i]=='2/3']=((S[i,1]+S[i,2])/2)*(-np.log10((af[i,1])*(af[i,2])))
-        db1[i][db1[i]=='3/2']=((S[i,1]+S[i,2])/2)*(-np.log10((af[i,1])*(af[i,2])))
-        db1[i][db1[i]=='2/4']=((S[i,1]+S[i,3])/2)*(-np.log10((af[i,1])*(af[i,3])))
-        db1[i][db1[i]=='4/2']=((S[i,1]+S[i,3])/2)*(-np.log10((af[i,1])*(af[i,3])))
-        db1[i][db1[i]=='2/5']=((S[i,1]+S[i,4])/2)*(-np.log10((af[i,1])*(af[i,4])))
-        db1[i][db1[i]=='5/2']=((S[i,1]+S[i,4])/2)*(-np.log10((af[i,1])*(af[i,4])))
-        db1[i][db1[i]=='2/6']=((S[i,1]+S[i,5])/2)*(-np.log10((af[i,1])*(af[i,5])))
-        db1[i][db1[i]=='6/2']=((S[i,1]+S[i,5])/2)*(-np.log10((af[i,1])*(af[i,5])))
-        db1[i][db1[i]=='2/7']=((S[i,1]+S[i,6])/2)*(-np.log10((af[i,1])*(af[i,6])))
-        db1[i][db1[i]=='7/2']=((S[i,1]+S[i,6])/2)*(-np.log10((af[i,1])*(af[i,6])))
-        db1[i][db1[i]=='2/8']=((S[i,1]+S[i,7])/2)*(-np.log10((af[i,1])*(af[i,7])))
-        db1[i][db1[i]=='8/2']=((S[i,1]+S[i,7])/2)*(-np.log10((af[i,1])*(af[i,7])))
-        db1[i][db1[i]=='2/9']=((S[i,1]+S[i,8])/2)*(-np.log10((af[i,1])*(af[i,8])))
-        db1[i][db1[i]=='9/2']=((S[i,1]+S[i,8])/2)*(-np.log10((af[i,1])*(af[i,8])))
-        db1[i][db1[i]=='3/3']=S[i,2]*(-np.log10((af[i,2])*(af[i,2])))
-        db1[i][db1[i]=='3/4']=((S[i,2]+S[i,3])/2)*(-np.log10((af[i,2])*(af[i,3])))
-        db1[i][db1[i]=='4/3']=((S[i,2]+S[i,3])/2)*(-np.log10((af[i,2])*(af[i,3])))
-        db1[i][db1[i]=='3/5']=((S[i,2]+S[i,4])/2)*(-np.log10((af[i,2])*(af[i,4])))
-        db1[i][db1[i]=='5/3']=((S[i,2]+S[i,4])/2)*(-np.log10((af[i,2])*(af[i,4])))
-        db1[i][db1[i]=='3/6']=((S[i,2]+S[i,5])/2)*(-np.log10((af[i,2])*(af[i,5])))
-        db1[i][db1[i]=='6/3']=((S[i,2]+S[i,5])/2)*(-np.log10((af[i,2])*(af[i,5])))
-        db1[i][db1[i]=='3/7']=((S[i,2]+S[i,6])/2)*(-np.log10((af[i,2])*(af[i,6])))
-        db1[i][db1[i]=='7/3']=((S[i,2]+S[i,6])/2)*(-np.log10((af[i,2])*(af[i,6])))
-        db1[i][db1[i]=='3/8']=((S[i,2]+S[i,7])/2)*(-np.log10((af[i,2])*(af[i,7])))
-        db1[i][db1[i]=='8/3']=((S[i,2]+S[i,7])/2)*(-np.log10((af[i,2])*(af[i,7])))
-        db1[i][db1[i]=='3/9']=((S[i,2]+S[i,8])/2)*(-np.log10((af[i,2])*(af[i,8])))
-        db1[i][db1[i]=='9/3']=((S[i,2]+S[i,8])/2)*(-np.log10((af[i,2])*(af[i,8])))
-        db1[i][db1[i]=='4/4']=S[i,3]*(-np.log10((af[i,3])*(af[i,3])))
-        db1[i][db1[i]=='4/5']=((S[i,3]+S[i,4])/2)*(-np.log10((af[i,3])*(af[i,4])))
-        db1[i][db1[i]=='4/6']=((S[i,3]+S[i,5])/2)*(-np.log10((af[i,3])*(af[i,5])))
-        db1[i][db1[i]=='4/7']=((S[i,3]+S[i,6])/2)*(-np.log10((af[i,3])*(af[i,6])))
-        db1[i][db1[i]=='4/8']=((S[i,3]+S[i,7])/2)*(-np.log10((af[i,3])*(af[i,7])))
-        db1[i][db1[i]=='4/9']=((S[i,3]+S[i,8])/2)*(-np.log10((af[i,3])*(af[i,8])))
-        db1[i][db1[i]=='5/5']=S[i,4]*(-np.log10((af[i,4])*(af[i,4])))
-        db1[i][db1[i]=='5/6']=((S[i,4]+S[i,5])/2)*(-np.log10((af[i,4])*(af[i,5])))
-        db1[i][db1[i]=='5/7']=((S[i,4]+S[i,6])/2)*(-np.log10((af[i,4])*(af[i,6])))
-        db1[i][db1[i]=='5/8']=((S[i,4]+S[i,7])/2)*(-np.log10((af[i,4])*(af[i,7])))
-        db1[i][db1[i]=='5/9']=((S[i,4]+S[i,8])/2)*(-np.log10((af[i,4])*(af[i,8])))
-        db1[i][db1[i]=='6/6']=S[i,5]*(-np.log10((af[i,5])*(af[i,5])))
-        db1[i][db1[i]=='6/7']=((S[i,5]+S[i,6])/2)*(-np.log10((af[i,5])*(af[i,6])))
-        db1[i][db1[i]=='6/8']=((S[i,5]+S[i,7])/2)*(-np.log10((af[i,5])*(af[i,7])))
-        db1[i][db1[i]=='6/9']=((S[i,5]+S[i,8])/2)*(-np.log10((af[i,5])*(af[i,8])))
-        db1[i][db1[i]=='7/7']=S[i,6]*(-np.log10((af[i,6])*(af[i,6])))
-        db1[i][db1[i]=='7/8']=((S[i,6]+S[i,7])/2)*(-np.log10((af[i,6])*(af[i,7])))
-        db1[i][db1[i]=='7/9']=((S[i,6]+S[i,8])/2)*(-np.log10((af[i,6])*(af[i,8])))
-        db1[i][db1[i]=='8/8']=S[i,7]*(-np.log10((af[i,7])*(af[i,7])))
-        db1[i][db1[i]=='8/9']=((S[i,7]+S[i,8])/2)*(-np.log10((af[i,7])*(af[i,8])))
-        db1[i][db1[i]=='9/9']=S[i,8]*(-np.log10((af[i,8])*(af[i,8])))
-        out1.append(db1[i])
-    out1=np.array(out1)
-    out1=np.nansum(nan_if(out1,'0.0'),axis=0)
-    gg = np.array([gene]*len(samples_header))
-    U = np.vstack((samples_header,out1,gg)).T
-    return U
-#%%
 
-if (np.isnan(scores).sum()) < (scores.shape[0]): #compute metascores if at least 1 variant
-    U = score_db(samples,scores,af0,af)
-    np.savetxt('./'+gene+'_'+CADD+'_matrix',U, fmt='%s', delimiter='\t')
+def score_db(
+    samples: np.array,
+    scores: np.array,
+    af: np.array,
+    gpu: bool,
+    gpu_threads_per_block: int,
+):
+    samples_int = index(samples)
+    db1 = np.zeros_like(samples, dtype=float)
+    if gpu:
+        db1 = get_scores_cuda(
+            scores, af, db1, samples_int, threads_per_block=gpu_threads_per_block
+        )
+    else:
+        db1 = get_score(scores, af, db1, samples_int)
+    db1[np.all(samples_int == np.asarray([0.0, 0.0], dtype="uint8"), axis=-1)] = 0.0
+    out1 = np.nansum(nan_if(db1, "0.0"), axis=0)
+    gg = np.array([gene] * len(samples_header))
+    U = np.vstack((samples_header, out1, gg)).T
+    return U
+
+
+if __name__ == "__main__":
+    args = parse_command_line_args()
+    meta_file = args.gene + ".meta"
+    gene = args.gene
+    cadd = args.cadd
+    gpu = args.gpu
+    gpu_threads_per_block = args.gpu_threads
+    data = read_meta_file(filepath=meta_file)
+    scores, af, data, samples_header = format_data(data=data)
+
+    if (np.isnan(scores).sum()) < (
+        scores.shape[0]
+    ):  # compute metascores if at least 1 variant
+        U = score_db(
+            samples=data,
+            scores=scores,
+            af=af,
+            gpu=gpu,
+            gpu_threads_per_block=gpu_threads_per_block,
+        )
+        np.savetxt(
+            "./" + args.gene + "_" + args.cadd + "_matrix", U, fmt="%s", delimiter="\t"
+        )
