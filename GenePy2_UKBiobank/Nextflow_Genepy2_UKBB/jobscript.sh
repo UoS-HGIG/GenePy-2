52f74c7eb5f8ad9fd6a68dbb7707a8479ff69e29
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2024-08-29 09:58:47 +0100

    Update jobscript.sh

--- GenePy2_UKBiobank/Nextflow_Genepy2_UKBB/jobscript.sh
+++ GenePy2_UKBiobank/Nextflow_Genepy2_UKBB/jobscript.sh
@@ -2,8 +2,8 @@
 SCRIPT=$(readlink -f .)
 vcf_directory=$1   ## input vcf path
 
-#chromosomes=$(ls $vcf_directory/*.vcf.gz | grep -oP 'chr[0-9XYM]+' | sort -u)
-chromosomes=$(ls $vcf_directory/*.vcf.gz | grep -oP 'chr6' | sort -u)
+chromosomes=$(ls $vcf_directory/*.vcf.gz | grep -oP 'chr[0-9M]+' | sort -u)
+#chromosomes=$(ls $vcf_directory/*.vcf.gz | grep -oP 'chr6' | sort -u)
 echo "all chromosomes : $chromosomes"
 for chrom in $chromosomes; do
     sbatch $SCRIPT/split_chr.sh $vcf_directory $chrom
